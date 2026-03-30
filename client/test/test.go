7715fa2f7111c597494b67d6b8946ac451f9e943
mlctrez <mlctrez@gmail.com>
2026-03-30 16:43:55 -0500

    add a more interesting test client

--- client/test/test.go
+++ client/test/test.go
@@ -2,36 +2,88 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"syscall"
 
 	"github.com/mlctrez/factop/client"
+	"github.com/mlctrez/factop/client/prototypes"
 	"github.com/mlctrez/factop/client/tiles"
+	"github.com/nats-io/nats.go"
+)
+
+const tileName = "lab-dark-1"
+const radius = 3
+
+// movePattern matches: [move] #<index> <name> to {x=<float>, y=<float>}
+var movePattern = regexp.MustCompile(
+	`\[move\] #\d+ \S+ to \{x=([+-]?\d+\.?\d*), y=([+-]?\d+\.?\d*)\}`,
 )
 
 func main() {
-	cl, err := client.Dial("nats://factorio")
-	if err != nil {
-		panic(err)
+	if !prototypes.ValidTile[tileName] {
+		fmt.Fprintf(os.Stderr, "invalid tile name: %s\n", tileName)
+		os.Exit(1)
 	}
-	defer cl.Close()
-	c := tiles.New(cl)
 
-	a := tiles.Area{X1: -5, Y1: -5, X2: 5, Y2: 5}
-	fill, err := c.Fill(a, "concrete", "")
-	if err != nil {
-		panic(err)
+	host := os.Getenv("FACTOP_HOST")
+	if host == "" {
+		host = "factorio"
 	}
-	fmt.Println(fill)
 
-	read, err := c.Read(a, "concrete", "")
+	conn, err := client.Dial(fmt.Sprintf("nats://%s", host))
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "dial: %v\n", err)
+		os.Exit(1)
 	}
-	fmt.Println(read)
+	defer conn.Close()
 
-	remove, err := c.Remove(a, "concrete", "")
+	tc := tiles.New(conn)
+
+	nc, err := nats.Connect(fmt.Sprintf("nats://%s", host))
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "nats connect: %v\n", err)
+		os.Exit(1)
 	}
-	fmt.Println(remove)
+	defer nc.Close()
 
+	fmt.Printf("watching for player movement, placing %s tiles (radius %d)...\n", tileName, radius)
+
+	_, err = nc.Subscribe("udp.incoming", func(msg *nats.Msg) {
+		matches := movePattern.FindSubmatch(msg.Data)
+		if matches == nil {
+			return
+		}
+		x, _ := strconv.ParseFloat(string(matches[1]), 64)
+		y, _ := strconv.ParseFloat(string(matches[2]), 64)
+
+		// Snap to tile coordinates and build area around player
+		tx := int(math.Floor(x))
+		ty := int(math.Floor(y))
+		area := tiles.Area{
+			X1: tx - radius,
+			Y1: ty - radius,
+			X2: tx + radius + 1,
+			Y2: ty + radius + 1,
+		}
+
+		result, fillErr := tc.Fill(area, tileName, "")
+		if fillErr != nil {
+			fmt.Fprintf(os.Stderr, "fill error: %v\n", fillErr)
+			return
+		}
+		fmt.Printf("player at {%.1f, %.1f} -> %s\n", x, y, result)
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "subscribe: %v\n", err)
+		os.Exit(1)
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+	fmt.Println("\nshutting down")
 }
